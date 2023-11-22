c26e419c2cac2591904521b58f7189ff764534a0
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2023-11-22 21:47:23 +0300

    Add random number

--- Net14Online/Net14OnlineConsole/Program.cs
+++ Net14Online/Net14OnlineConsole/Program.cs
@@ -1,26 +1,94 @@
-﻿var number = 42;
-
-Console.WriteLine("It's game: Guess the number");
-Console.WriteLine("Enter any number");
-
-int userGuess;
-bool isUserGood;
-do
+﻿internal class Program
 {
-    var userNumberString = Console.ReadLine();
-    isUserGood = int.TryParse(userNumberString, out userGuess);
-    if (!isUserGood)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("It's not a number. Enter NUMBER");
+        var minNumber = 150;
+        var maxNumber = 2000;
+        var totalAttemptCount = CalculateAttempCount(minNumber, maxNumber);
+        var random = new Random();
+        var theNumber = random.Next(minNumber, maxNumber);
+        var isWin = false;
+
+        Console.WriteLine("It's game: Guess the number");
+
+        for (var attempt = 0; attempt < totalAttemptCount; attempt++)
+        {
+            int userGuess;
+            
+            Console.WriteLine($"Attempt: {attempt + 1}/{totalAttemptCount}. Range [{minNumber}, {maxNumber}] Enter any number");
+
+            userGuess = GetUserGuess(minNumber, maxNumber);
+
+            if (theNumber == userGuess)
+            {
+                isWin = true;
+                break;
+            }
+            else
+            {
+                if (theNumber < userGuess)
+                {
+                    maxNumber = userGuess - 1;
+                    Console.WriteLine(" < ");
+                }
+                else
+                {
+                    minNumber = userGuess + 1;
+                    Console.WriteLine(" > ");
+                }
+            }
+        }
+
+        if (isWin)
+        {
+            Console.WriteLine("Win!!!");
+        }
+        else
+        {
+            Console.WriteLine("Loose");
+        }
+
+        Console.WriteLine("The end");
     }
-} while (!isUserGood);
 
-if (number == userGuess)
-{
-    Console.WriteLine("Win!!!");
-}
-else
-{
-    Console.WriteLine("Looser");
-}
+    private static int CalculateAttempCount(int minNumber, int maxNumber)
+    {
+        var numberCount = maxNumber - minNumber + 1;
+        var totalAttemptCount = 1;
+        var number = 1;
+        while (number < numberCount)
+        {
+            totalAttemptCount++;
+            number = number * 2;
+        }
 
+        return totalAttemptCount;
+    }
+
+    private static int GetUserGuess(int min, int max)
+    {
+        bool isUserGood;
+        int userGuess;
+        do
+        {
+            var userNumberString = Console.ReadLine();
+            isUserGood = int.TryParse(userNumberString, out userGuess);
+            if (!isUserGood)
+            {
+                Console.WriteLine("It's not a number. Enter NUMBER");
+            }
+            if (userGuess < min)
+            {
+                Console.WriteLine($"Can't be less then {min}");
+                isUserGood = false;
+            }
+            if (userGuess > max)
+            {
+                Console.WriteLine($"Can't be more then {max}");
+                isUserGood = false;
+            }
+        } while (!isUserGood);
+
+        return userGuess;
+    }
+}
