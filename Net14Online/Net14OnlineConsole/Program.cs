5fa69864532950cc76db2e02ddd37e4dd7440527
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2023-11-22 20:22:10 +0300

    Guess the number. One attempt

---
+++ Net14Online/Net14OnlineConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿var number = 42;
+
+Console.WriteLine("It's game: Guess the number");
+Console.WriteLine("Enter any number");
+
+int userGuess;
+bool isUserGood;
+do
+{
+    var userNumberString = Console.ReadLine();
+    isUserGood = int.TryParse(userNumberString, out userGuess);
+    if (!isUserGood)
+    {
+        Console.WriteLine("It's not a number. Enter NUMBER");
+    }
+} while (!isUserGood);
+
+if (number == userGuess)
+{
+    Console.WriteLine("Win!!!");
+}
+else
+{
+    Console.WriteLine("Looser");
+}
+
