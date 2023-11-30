18915b2481b6d4a38faf41099974da593d106aee
Askar Turaev <Aturaev42@gmail.com>
2023-11-30 22:09:03 +0300

    Task_2 add diamond, comment  ground and create new ground generation

--- Net14Online/Maze/LevelStaff/LevelBuilder.cs
+++ Net14Online/Maze/LevelStaff/LevelBuilder.cs
@@ -30,19 +30,35 @@ namespace Maze.LevelStaff
         }
 
 
-        private void BuildGroundRandom()
+        private void BuildGroundV1()
         {
-            for (int i = 0; i < 15; i++)
+            int[] specificX = { 2, 4, 6 };
+            int[] specificY = { 3, 7, 9 };
+            foreach (int x in specificX)
             {
-                var randomX = _random.Next(_level.Width);
-                var randomY = _random.Next(_level.Height);
+                foreach (int y in specificY)
+                {
+                    var existingCell = _level.Cells.First(cell => cell.CoordinateX == x && cell.CoordinateY == y);
 
-                var randomWall = _level.Cells.First(x => x.CoordinateX == randomX && x.CoordinateY == randomY);
-                var ground = new Ground(randomX, randomY, _level);
-
-                _level.Cells.Remove(randomWall);
-                _level.Cells.Add(ground);
+                    if (existingCell == null)
+                    {
+                        var ground = new Ground(x, y, _level);
+                        _level.Cells.Add(ground);
+                    }
+                }
             }
+            //!!Код закомменчен в связи с заданием, ветка task_2, 30.11.2023!!
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    var randomX = _random.Next(_level.Width);
+            //    var randomY = _random.Next(_level.Height);
+
+            //    var randomWall = _level.Cells.First(x => x.CoordinateX == randomX && x.CoordinateY == randomY);
+            //    var ground = new Ground(randomX, randomY, _level);
+
+            //    _level.Cells.Remove(randomWall);
+            //    _level.Cells.Add(ground);
+            //}
         }
 
         private void BuildWall()
@@ -55,6 +71,31 @@ namespace Maze.LevelStaff
 
                     _level.Cells.Add(cell);
                 }
+            }
+        }
+        private void Diamond() 
+        {
+            foreach (var ground in _level.Cells)
+            {
+                int[] moveX = { 1, 0, -1 };
+                int[] moveY = { 0, 1, -1 };
+                foreach (int x in moveX)
+                { 
+                    foreach(int y in moveY)
+                    {
+                        int newX = ground.CoordinateX + x;
+                        int newY = ground.CoordinateY + y;
+                        var existingCell = _level.Cells.First(cell => cell.CoordinateX == x && cell.CoordinateY == y);
+
+                        if (existingCell == null)
+                        {
+                            var diamond = new Diamond(newX, newY, _level); 
+                            _level.Cells.Add(diamond);
+                        }
+                    }
+                    
+                }
+                
             }
         }
     }
