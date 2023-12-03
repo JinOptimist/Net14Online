7cc7a204c1e4c517db176a1281b4fded170ac5d6
SashaKuznetsova <Sashaa_ku@mail.ru>
2023-12-03 17:48:59 +0100

    Task_18 added cage generation

--- Net14Online/Maze/LevelStaff/LevelBuilder.cs
+++ Net14Online/Maze/LevelStaff/LevelBuilder.cs
@@ -29,7 +29,28 @@ namespace Maze.LevelStaff
             return _level;
         }
 
+        public Level BuildV18(int width = 10, int height = 5, int seedForRandom = -1)
+        {
+            if (seedForRandom > 0)
+            {
+                _random = new Random(seedForRandom);
+            }
+            else
+            {
+                _random = new Random();
+            }
 
+            _level = new Level();
+
+            _level.Width = width;
+            _level.Height = height;
+
+            BuildWall();
+            BuildGroundV18();
+            BuildCage();
+
+            return _level;
+        }
         private void BuildGroundRandom()
         {
             for (int i = 0; i < 15; i++)
@@ -57,5 +78,45 @@ namespace Maze.LevelStaff
                 }
             }
         }
+
+        private void BuildGroundV18()
+        {
+            for (int y = 0; y < _level.Height; y++)
+            {
+                var midX = _level.Width / 2;
+
+                var cell = _level.Cells.First(x => x.CoordinateX == midX && x.CoordinateY == y);
+                var ground = new Ground(midX, y, _level);
+
+                _level.Cells.Remove(cell);
+                _level.Cells.Add(ground);
+            }
+            for (int x = 0; x < _level.Width; x++)
+            {
+                var midY = _level.Height/ 2;
+
+                var cell = _level.Cells.First(c => c.CoordinateY == midY && c.CoordinateX == x);
+                var ground = new Ground( x, midY, _level);
+
+                _level.Cells.Remove(cell);
+                _level.Cells.Add(ground);
+
+            }
+        }
+
+        private void BuildCage()
+        {
+            var cells = _level.Cells.Where(c => c.Symbol == "." && (c.CoordinateX == _level.Width-1 || c.CoordinateX == 0)).ToList();      
+             foreach(var cell in cells)
+            {
+                var cage = new Cage(cell.CoordinateX, cell.CoordinateY, _level);
+
+                _level.Cells.Remove(cell);
+                _level.Cells.Add(cage);
+            }  
+        
+        }
+
     }
 }
+   
