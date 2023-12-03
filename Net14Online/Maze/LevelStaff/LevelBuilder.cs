4d9f331959557bedaa6998df8b27fd7fd9d05c95
Pavel-Karpov-2002 <79276746+Pavel-Karpov-2002@users.noreply.github.com>
2023-12-03 18:24:33 +0300

    Add new generation

--- Net14Online/Maze/LevelStaff/LevelBuilder.cs
+++ Net14Online/Maze/LevelStaff/LevelBuilder.cs
@@ -29,6 +29,23 @@ namespace Maze.LevelStaff
             return _level;
         }
 
+        public Level BuildV1(int width = 10, int height = 5, int seedForRandom = -1, int numberOfSecrets = 2)
+        {
+            if (seedForRandom > 0)
+                _random = new Random(seedForRandom);
+            else
+                _random = new Random();
+
+            _level = new Level();
+
+            _level.Width = width;
+            _level.Height = height;
+
+            BuildWall();
+            BuildSecret(numberOfSecrets);
+
+            return _level;
+        }
 
         private void BuildGroundRandom()
         {
@@ -45,6 +62,21 @@ namespace Maze.LevelStaff
             }
         }
 
+        private void BuildGround((int x, int y) position)
+        {
+            if (position.x < 0 || position.y < 0)
+                return;
+
+            var oldCell = _level.Cells.FirstOrDefault(x => x.CoordinateX == position.x && x.CoordinateY == position.y);
+            var ground = new Ground(position.x, position.y, _level);
+
+            if (oldCell is Wall)
+            {
+                _level.Cells.Remove(oldCell);
+                _level.Cells.Add(ground);
+            }
+        }
+
         private void BuildWall()
         {
             for (int x = 0; x < _level.Width; x++)
@@ -56,6 +88,54 @@ namespace Maze.LevelStaff
                     _level.Cells.Add(cell);
                 }
             }
+        }
+
+        private void BuildSecret(int numberOfSecrets)
+        {
+            for (int i = 0; i < numberOfSecrets; i++)
+            {
+                var randomX = _random.Next(1, _level.Width - 1);
+                var randomY = _random.Next(1, _level.Height - 1);
+                Secret secret = InitializeSecret((randomX, randomY));
+                BuildPaths((_level.Width / 2, _level.Height / 2), (secret.CoordinateX, secret.CoordinateY));
+            }
+        }
+
+        private Secret InitializeSecret((int x, int y) position)
+        {
+            var oldCell = _level.Cells.First(x => x.CoordinateX == position.x && x.CoordinateY == position.y);
+            var secret = new Secret(position.x, position.y, _level);
+
+            _level.Cells.Remove(oldCell);
+            _level.Cells.Add(secret);
+
+            return secret;
+        }
+
+        private void BuildPaths((int x, int y) startPosition, (int x, int y) endPosition)
+        {
+            BuildGround((startPosition.x, startPosition.y));
+
+            int direction = 0;
+            if ((direction = GetDirection((startPosition.x, endPosition.x))) != 0)
+            {
+                BuildPaths((startPosition.x + direction, startPosition.y), (endPosition.x, endPosition.y));
+                return;
+            }
+
+            if ((direction = GetDirection((startPosition.y, endPosition.y))) != 0)
+            {
+                BuildPaths((startPosition.x, startPosition.y + direction), (endPosition.x, endPosition.y));
+                return;
+            }
+        }
+
+        private int GetDirection((int startPos, int endPos) position)
+        {
+            int direction = 0;
+            if (position.startPos != position.endPos)
+                direction = position.startPos < position.endPos ? 1 : -1;
+            return direction;
         }
     }
 }
