ace7e681326a05f8ee7891e6a30f31da45d07f73
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2023-12-08 11:16:49 +0300

    Fix a lot of problems. Now build is working

--- Net14Online/Maze/Cells/gold.cs
+++ Net14Online/Maze/Cells/gold.cs
@@ -1,24 +1,20 @@
-﻿using Maze.LevelStaff;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Maze.Cells.Creatures;
+using Maze.LevelStaff;
 
 namespace Maze.Cells
 {
-     public  class Gold : BaseCell
+    public  class Gold : BaseCell
     {
-        private Gold goldx;
-        private Gold goldy;
-        private Level level;
-
         public Gold(int coordinateX, int coordinateY, Level level) : base(coordinateX, coordinateY, level)
         {
 
         }
 
-
         public override string Symbol => "+";
+
+        public override bool Step(BaseCreature creature)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
