9a148991534eecd6354ccd46c1bfb432c17b442f
AASaln89 <94790664+AASaln89@users.noreply.github.com>
2023-12-09 14:43:44 +0300

    Task_35-36 + Chadged class House. Created the method ShowStats in class LevelDrawer Created the method SetHouseColor

--- Net14Online/Maze/Cells/House.cs
+++ Net14Online/Maze/Cells/House.cs
@@ -1,25 +1,38 @@
 ﻿using Maze.Cells.Creatures;
 using Maze.LevelStaff;
+using System.Reflection.Emit;
 
 namespace Maze.Cells
 {
     public class House : BaseCell
     {
+        /// <summary>
+        /// Property for indicating objects of class House by Color
+        /// </summary>
+        public static ConsoleColor HouseColor { get; } = ConsoleColor.White;
+        /// <summary>
+        /// Property for indicating objects of class BaseCell by Color. It depends on landscape of current map and should get value from closed cells.
+        /// </summary>
+        public ConsoleColor CellColor { get; set; }
+
         public House(int coordinateX, int coordinateY, Level level) : base(coordinateX, coordinateY, level)
         {
         }
 
         public override string Symbol => "^";
 
-        public static void ChangeGroundToHouse(List<Ground> grounds, Level level)
+        public static void ChangeGroundToHouse(Level level)
         {
-            for (int i = 0; i < grounds.Count; i = i + 4)
+            for (int i = 0; i < level.Cells.Count; i = i + 4)
             {
-                var randomGround = level.Cells.First(x => x.CoordinateX == grounds[i].CoordinateX && x.CoordinateY == grounds[i].CoordinateY);
-                House house = new House(grounds[i].CoordinateX, grounds[i].CoordinateY, level);
+                if (level.Cells[i] is Ground)
+                {
+                    var randomGround = level.Cells.First(x => x.CoordinateX == level.Cells[i].CoordinateX && x.CoordinateY == level.Cells[i].CoordinateY);
+                    House house = new House(level.Cells[i].CoordinateX, level.Cells[i].CoordinateY, level);
 
-                level.Cells.Remove(randomGround);
-                level.Cells.Add(house);
+                    level.Cells.Remove(randomGround);
+                    level.Cells.Add(house);
+                }             
             }
         }
 
@@ -27,6 +40,32 @@ namespace Maze.Cells
         {
             creature.Hp *= 2;
             return true;
+        }
+
+        public static void SetHouseColor(BaseCell cell, ConsoleColor consoleColor)
+        {
+            if (cell is House)
+            {
+                Console.SetCursorPosition(cell.CoordinateX, cell.CoordinateY);
+                Console.ForegroundColor = HouseColor;
+                Console.BackgroundColor = consoleColor;
+            }
+        }
+        /// <summary>
+        /// Just in case when somebody needs to check object color of class House and may be use it
+        /// </summary>
+        /// <returns></returns>
+        public ConsoleColor GetCurrentObjectColor()
+        {
+            return HouseColor;
+        }
+        /// <summary>
+        /// Just in case when somebody needs to check cell color of class House and may be use or change it
+        /// </summary>
+        /// <returns></returns>
+        public ConsoleColor GetCurrentCellColor()
+        {
+            return CellColor;
         }
     }
 }
--- Net14Online/Maze/LevelStaff/LevelDrawer.cs
+++ Net14Online/Maze/LevelStaff/LevelDrawer.cs
@@ -1,4 +1,6 @@
-﻿namespace Maze.LevelStaff
+﻿using Maze.Cells.Creatures;
+
+namespace Maze.LevelStaff
 {
     public class LevelDrawer
     {
@@ -9,11 +11,20 @@
                 Console.SetCursorPosition(cell.CoordinateX, cell.CoordinateY);
                 Console.Write(cell.Symbol);
             }
+            
+            ShowStats(level);
 
             Console.SetCursorPosition(level.Hero.CoordinateX, level.Hero.CoordinateY);
             Console.Write(level.Hero.Symbol);
 
             //Console.ReadLine();
+        }
+
+        private void ShowStats(Level level)
+        {
+            Console.SetCursorPosition(level.Width = 0, level.Height +1);
+            Console.WriteLine($"HP level.Hero.Name: {level.Hero.Hp}");
+            Console.WriteLine($"Money level.Hero.Name: {level.Hero.Money}");
         }
     }
 }
