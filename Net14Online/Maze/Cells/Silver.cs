5fd6e77a7048ee9fbb93f7a3e97e35694b3a3e65
ZhenyaNorseev <151774713+ZhenyaNorseev@users.noreply.github.com>
2023-12-11 15:07:10 +0300

    Add Silver Class Delet gold.cs add method BuildSilver add method BuildHeroSilver Fix problem Program.cs

---
+++ Net14Online/Maze/Cells/Silver.cs
@@ -0,0 +1,24 @@
+﻿using Maze.Cells.Creatures;
+using Maze.LevelStaff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze.Cells 
+{
+    internal class Silver : BaseCell
+    {
+        public Silver(int coordinateX, int coordinateY, Level level) : base(coordinateX, coordinateY, level)
+        {
+        }
+
+        public override string Symbol => "+";
+
+        public override bool Step(BaseCreature creature)
+        {
+            return true;
+        }
+    }
+}
--- Net14Online/Maze/Program.cs
+++ Net14Online/Maze/Program.cs
@@ -1,6 +1,13 @@
 ﻿using Maze.ConsolePlay;
+using Maze.LevelStaff;
+
+var builder = new LevelBuilder();
+var level = builder.BuildV0(12, 7);
+
 
 // player push the button
 var consoleController = new ConsoleController();
 consoleController.Play();
 
+var drawer = new LevelDrawer();
+drawer.Draw(level);
