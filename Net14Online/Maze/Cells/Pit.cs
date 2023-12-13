c85e76c5cd195436cc982bac61a119af46689a80
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2023-12-13 18:31:48 +0300

    Add Pit with generation

---
+++ Net14Online/Maze/Cells/Pit.cs
@@ -0,0 +1,19 @@
+﻿using Maze.Cells.Creatures;
+using Maze.LevelStaff;
+
+namespace Maze.Cells
+{
+    public class Pit : BaseCell
+    {
+        public Pit(int coordinateX, int coordinateY, Level level) : base(coordinateX, coordinateY, level)
+        {
+        }
+
+        public override string Symbol => "~";
+
+        public override bool Step(BaseCreature creature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
