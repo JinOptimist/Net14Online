5c40e3d67ad0cd3a2d9d79717f1e6ca397af0945
AASaln89 <94790664+AASaln89@users.noreply.github.com>
2023-12-13 13:33:42 +0300

    Remove static methods

--- Net14Online/Maze/Cells/House.cs
+++ Net14Online/Maze/Cells/House.cs
@@ -11,21 +11,6 @@ namespace Maze.Cells
 
         public override string Symbol => "^";
 
-        public static void ChangeGroundToHouse(Level level)
-        {
-            for (int i = 0; i < level.Cells.Count; i = i + 4)
-            {
-                if (level.Cells[i] is Ground)
-                {
-                    var randomGround = level.Cells.First(x => x.CoordinateX == level.Cells[i].CoordinateX && x.CoordinateY == level.Cells[i].CoordinateY);
-                    House house = new House(level.Cells[i].CoordinateX, level.Cells[i].CoordinateY, level);
-
-                    level.Cells.Remove(randomGround);
-                    level.Cells.Add(house);
-                }
-            }
-        }
-
         public override bool Step(BaseCreature creature)
         {
             if (creature is Hero)
