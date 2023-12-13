b413d3c09768054131a5f321794032a94adfc29a
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2023-12-13 18:24:23 +0300

    Add zombie

---
+++ Net14Online/Maze/Cells/Creatures/Zombie.cs
@@ -0,0 +1,24 @@
+﻿using Maze.LevelStaff;
+
+namespace Maze.Cells.Creatures
+{
+    public class Zombie : BaseCreature
+    {
+        public override string Symbol => "Z";
+
+        public Zombie(int coordinateX, int coordinateY, Level level) : base(coordinateX, coordinateY, level)
+        {
+
+        }
+
+        public override BaseCell ChooseCellToStep()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Step(BaseCreature creature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
