b5538af668d370066f3d0d32c456180dbcbab532
Vasileva Maria <buziuka@yandex.ru>
2023-12-18 18:04:04 +0100

    Update CageTest.cs
    add result argument to the test

--- Net14Online/Maze.Tests/Cells/CageTest.cs
+++ Net14Online/Maze.Tests/Cells/CageTest.cs
@@ -13,41 +13,33 @@ namespace Maze.Tests.Cells
 {
     public class CageTest
     {
-
-        //when creature steps for the every odd time (1, 3, 5 etc), cage must not let it go, when creature steps for the every even time, cage must let it go
         [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, false)]
+        [TestCase(4, true)]
+        /// <summary>
+        ///  when creature steps for the every odd time (1, 3, 5 etc), cage must not let it go, when creature steps for the every even time, cage must let it go
+        /// </summary>
+        /// <param name="numberOfSteps"></param>
+        /// <param name="result"></param>
 
-
-        public void CageEvenNumberOfStepsTest(int numberOfSteps)
+        public void CageEvenNumberOfStepsTest(int numberOfSteps, bool result)
         {
             // Preparation
             var levelMock = new Mock<ILevel>();
             var creatureMock = new Mock<IBaseCreature>();
-
             var cage = new Cage(0, 0, levelMock.Object);
 
             // act
-            for (int step  = 0; step  < numberOfSteps - 1; step ++)
+            for (var step = 0; step < numberOfSteps - 1; step++)
             {
                 cage.Step(creatureMock.Object);
             }
             var answer = cage.Step(creatureMock.Object);
 
             // assert
-            if (numberOfSteps % 2 == 0)
-            {
-                Assert.That(answer, Is.True, "Cage must let creature go");
-            }
-            else
-            {
-                Assert.That(answer, Is.False, "Cage must not let creature go");
-            }
-
-
+            Assert.That(answer, Is.EqualTo(result), "Cage must not let creature go");
         }
     }
 }
