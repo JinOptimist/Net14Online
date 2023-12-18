d7625b251cc8c5a68b585545eb11de6efc237450
AASaln89 <94790664+AASaln89@users.noreply.github.com>
2023-12-18 20:07:26 +0300

    Fix

--- Net14Online/Maze/Cells/Creatures/Interfaces/IHero.cs
+++ Net14Online/Maze/Cells/Creatures/Interfaces/IHero.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Maze.Cells.Creatures.Interfaces
+﻿namespace Maze.Cells.Creatures.Interfaces
 {
     public interface IHero : IBaseCreature
     {
