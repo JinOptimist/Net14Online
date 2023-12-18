46be996e9b1c5c6965adb6ee842e7d23971947ae
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2023-12-18 18:18:05 +0300

    add Step logic for class Pit

--- Net14Online/Maze/Cells/Pit.cs
+++ Net14Online/Maze/Cells/Pit.cs
@@ -1,4 +1,4 @@
-﻿using Maze.Cells.Creatures;
+﻿using Maze.Cells.CellInterfaces;
 using Maze.Cells.Creatures.Interfaces;
 using Maze.LevelStaff;
 
@@ -6,15 +6,46 @@ namespace Maze.Cells
 {
     public class Pit : BaseCell
     {
+        private readonly Level _level;
+        private Random _random = new Random();
+
         public Pit(int coordinateX, int coordinateY, Level level) : base(coordinateX, coordinateY, level)
         {
+            _level = level;
         }
 
         public override string Symbol => "~";
 
         public override bool Step(IBaseCreature creature)
         {
-            throw new NotImplementedException();
+            var pits = _level.Cells.Where(x => x is Pit).ToList();
+            var randomPitNumber = _random.Next(pits.Count);
+            var randomPit = pits[randomPitNumber];
+
+            if (CheckPosition(creature, randomPit) && randomPitNumber < pits.Count)
+            {
+                randomPit = pits[randomPitNumber + 1];
+            }
+            else if (CheckPosition(creature, randomPit) && randomPitNumber == pits.Count)
+            {
+                randomPit = pits[randomPitNumber - 1];
+            }
+
+            creature.CoordinateX = randomPit.CoordinateX;
+            creature.CoordinateY = randomPit.CoordinateY;
+
+            return true;
+        }
+
+        private bool CheckPosition(IBaseCreature creature, IBaseCell pit)
+        {
+            var result = false;
+
+            if (pit.CoordinateX != creature.CoordinateX && pit.CoordinateY != creature.CoordinateY)
+            {
+                result = true;
+            }
+            return result;
         }
     }
 }
