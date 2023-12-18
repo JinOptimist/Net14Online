9e3b51321337334a977f2ab7e4ed3b4fd85001ae
Artur Haurusik <37577860+Artifex-ag@users.noreply.github.com>
2023-12-18 23:22:17 +0300

    Changed test cases and added a case that contains a mistake. Also changed a name of the variable that has a value of the dummy object 'Mock<IBaseCreature>' from 'heroMock' to 'creatureMock'. New one is more suitable in this case

--- Net14Online/Maze.Tests/Cells/Creatures/Terminator3000Test.cs
+++ Net14Online/Maze.Tests/Cells/Creatures/Terminator3000Test.cs
@@ -14,20 +14,21 @@ namespace Maze.Tests.Cells.Creatures
     public class Terminator3000Test
     {
         [Test]
-        [TestCase(10, 5)]
-        [TestCase(20, 10)]
+        [TestCase(21, 7)]
+        [TestCase(36, 12)]
+        [TestCase(25, 8)]
         public void Terminator3000_Step_CheckCreatureMoney(int moneyBefore, int moneyAfter)
         {
             var levelMock = new Mock<ILevel>();
-            var heroMock = new Mock<IBaseCreature>();
-            heroMock.SetupProperty(x => x.Money);
-            heroMock.Object.Money = moneyBefore;
+            var creatureMock = new Mock<IBaseCreature>();
+            creatureMock.SetupProperty(x => x.Money);
+            creatureMock.Object.Money = moneyBefore;
 
             var terminator3000 = new Terminator3000(0, 0, levelMock.Object);
 
-            var answer = terminator3000.Step(heroMock.Object);
+            var answer = terminator3000.Step(creatureMock.Object);
 
-            Assert.That(heroMock.Object.Money, Is.EqualTo(moneyAfter));
+            Assert.That(creatureMock.Object.Money, Is.EqualTo(moneyAfter));
         }
     }
 }
