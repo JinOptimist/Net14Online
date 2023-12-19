2a99b94a6af469a9be15174c577b08f8c7db890b
GerardoGerardi <73798494+GerardoGerardi@users.noreply.github.com>
2023-12-20 02:17:39 +0300

    Add Witch

---
+++ Net14Online/Maze/Cells/Creatures/Witch.cs
@@ -0,0 +1,42 @@
+﻿using Maze.Cells.CellInterfaces;
+using Maze.Cells.Creatures.Interfaces;
+using Maze.LevelStaff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze.Cells.Creatures
+{
+    public class Witch : BaseCreature
+    {
+        private Random _random = new Random();
+        public Witch(int coordinateX, int coordinateY, Level level, ConsoleColor color = ConsoleColor.DarkCyan)
+            : base(coordinateX, coordinateY, level, color)
+        {
+        }
+
+        public override string Symbol => "W";
+
+        public override IBaseCell ChooseCellToStep()
+        {
+            var cells = Level.GetNearCells<BaseCell>(this);
+            var randomInex = _random.Next(cells.Count);
+            var cell = cells[randomInex];
+            return cell;
+        }
+
+        public override bool Step(IBaseCreature creature)
+        {
+
+            if (creature is Hero)
+            {
+                creature.Hp -= 2;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
