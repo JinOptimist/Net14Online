5b6a7f5a56a5d2bffe1e5552b2fcd7d263345620
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2023-12-20 18:56:19 +0300

    update

--- Net14Online/Maze/Cells/Pit.cs
+++ Net14Online/Maze/Cells/Pit.cs
@@ -6,37 +6,35 @@ namespace Maze.Cells
 {
     public class Pit : BaseCell
     {
-<<<<<<< Updated upstream
-        private readonly Level _level;
         private Random _random = new Random();
 
-        public Pit(int coordinateX, int coordinateY, Level level) : base(coordinateX, coordinateY, level)
-=======
         public Pit(int coordinateX, int coordinateY, ILevel level) : base(coordinateX, coordinateY, level)
->>>>>>> Stashed changes
         {
-            _level = level;
+
         }
 
         public override string Symbol => "~";
 
         public override bool Step(IBaseCreature creature)
         {
-            var pits = _level.Cells.Where(x => x is Pit).ToList();
-            var randomPitNumber = _random.Next(pits.Count);
-            var randomPit = pits[randomPitNumber];
-
-            if (CheckPosition(creature, randomPit) && randomPitNumber < pits.Count)
+            var pits = Level.Cells.Where(x => x is Pit).ToList();
+            if (pits.Count > 1)
             {
-                randomPit = pits[randomPitNumber + 1];
-            }
-            else if (CheckPosition(creature, randomPit) && randomPitNumber == pits.Count)
-            {
-                randomPit = pits[randomPitNumber - 1];
-            }
+                var randomPitNumber = _random.Next(pits.Count);
+                var randomPit = pits[randomPitNumber];
 
-            creature.CoordinateX = randomPit.CoordinateX;
-            creature.CoordinateY = randomPit.CoordinateY;
+                if (CheckPosition(creature, randomPit) && randomPitNumber < pits.Count)
+                {
+                    randomPit = pits[randomPitNumber + 1];
+                }
+                if (CheckPosition(creature, randomPit) && randomPitNumber == pits.Count)
+                {
+                    randomPit = pits[randomPitNumber - 1];
+                }
+
+                creature.CoordinateX = randomPit.CoordinateX;
+                creature.CoordinateY = randomPit.CoordinateY;
+            }
 
             return true;
         }
