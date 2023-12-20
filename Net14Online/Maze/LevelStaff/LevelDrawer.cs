4b004d7339a15d78ccf12fc9853ccb346480d1a7
AntonKupalov <137334848+AntonKupalov@users.noreply.github.com>
2023-12-20 08:09:12 -0600

    Task_148 Add drawing color cells

--- Net14Online/Maze/LevelStaff/LevelDrawer.cs
+++ Net14Online/Maze/LevelStaff/LevelDrawer.cs
@@ -9,23 +9,32 @@ namespace Maze.LevelStaff
             foreach (var cell in level.Cells)
             {
                 Console.SetCursorPosition(cell.CoordinateX, cell.CoordinateY);
+                DrawingColorCells(cell.Color);
                 Console.Write(cell.Symbol);
             }
 
             foreach (var creature in level.Creatures)
             {
                 Console.SetCursorPosition(creature.CoordinateX, creature.CoordinateY);
+                DrawingColorCells(creature.Color);
                 Console.Write(creature.Symbol);
             }
 
             if (level.Hero != null)
             {
                 Console.SetCursorPosition(level.Hero.CoordinateX, level.Hero.CoordinateY);
+                DrawingColorCells(level.Hero.Color);
                 Console.Write(level.Hero.Symbol);
             }
 
 
             //Console.ReadLine();
         }
+
+        private void DrawingColorCells(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+        
     }
 }
