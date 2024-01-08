533aecffcbe58bc1c3ce34ede19a007fad573e08
Pavel-Karpov-2002 <79276746+Pavel-Karpov-2002@users.noreply.github.com>
2024-01-09 01:36:13 +0300

    Add backend

---
+++ Net14Online/Net14Web/Models/Movies/PersoneViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Net14Web.Models.Movies
+{
+    public class PersoneViewModel
+    {
+        public string Login {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string AvatarUrl {  get; set; }
+        public List<CommentsInPersoneAccount> Comments { get; set; }
+    }
+}
