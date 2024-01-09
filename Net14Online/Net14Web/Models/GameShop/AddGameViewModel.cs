1089b3d7fa35d24df960953e1f8feb06e429b485
Alexandr <alexandrglazko@mail.ru>
2024-01-10 02:28:49 +0300

    Made homework

---
+++ Net14Online/Net14Web/Models/GameShop/AddGameViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Net14Web.Models.GameShop
+{
+    public class AddGameViewModel
+    {
+        public string? Name { get; set; }
+
+        public string? PosterUrl { get; set; }
+
+        public string? Genre { get; set; }
+    }
+}
