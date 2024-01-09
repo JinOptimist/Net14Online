08d81bb343100accdfc0518db5324ff2a8f99159
Askar Turaev <Aturaev42@gmail.com>
2024-01-09 22:50:01 +0300

    Ретро консоль

---
+++ Net14Online/Net14Web/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net14Web.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Net14Online/Net14Web/Controllers/RetroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net14Web.Models;
+using System.Diagnostics;
+
+namespace Net14Web.Controllers
+{
+    public class RetroController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+   
+    }
+}
