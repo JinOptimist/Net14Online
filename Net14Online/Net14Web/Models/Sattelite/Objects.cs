479e8bef05f5fba661027ab14869b402c993c857
StanislavYan <stanislaff94@mail.ru>
2024-01-10 21:46:23 +0700

    AddMainPage

---
+++ Net14Online/Net14Web/Models/Sattelite/Objects.cs
@@ -0,0 +1,12 @@
+﻿namespace Net14Web.Models.Sattelite
+{
+    public class Objects
+    {
+        public string Name { get; set; }
+        public string? Type { get; set; }
+        public string? IconURL { get; set; }
+
+
+    }
+}
+
