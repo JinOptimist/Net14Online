54a4e561edbc7e97d806e4f2b4226a54cec42df9
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2024-01-10 19:42:27 +0300

    Add LifeScore

---
+++ Net14Online/Net14Web/Models/LifeScore/LifeScoreViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Net14Web.Models.LifeScore
+{
+    public class LifeScoreViewModel
+    {
+        public List<TeamViewModel> Teams { get; set; }
+        public List<GameViewModel> Games { get; set; }
+        
+    }
+}
