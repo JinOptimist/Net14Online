0d35333c93fe92d1227e80b266be3868304e5eb9
AASaln89 <94790664+AASaln89@users.noreply.github.com>
2024-01-10 19:03:44 +0300

    Task_183

---
+++ Net14Online/Net14Web/Controllers/ManagmentCompanyController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net14Web.Models.ManagmentCompany;
+
+namespace Net14Web.Controllers
+{
+    public class ManagmentCompanyController : Controller
+    {
+        public static List<UserViewModel> userViewModels = new List<UserViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(userViewModels);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registrstion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrstion(AddUserViewModel userViewModel)
+        {
+            var newUser = new UserViewModel
+            {
+                Name = userViewModel.Name,
+                PermissionLevel = "Пользователь",
+            };
+
+            userViewModels.Add(newUser);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
