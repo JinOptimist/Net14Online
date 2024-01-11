98ca4970151517b9660e8882de5dbf93ba1031b5
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-01-11 10:08:08 +0400

    add 2 action

---
+++ Net14Online/Net14Web/Controllers/InvestPortfolioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net14Web.Models.InvestPortfolio;
+
+namespace Net14Web.Controllers
+{
+    public class InvestPortfolioController : Controller
+    {
+        public static List<StockViewModel> stockViewModels = new List<StockViewModel>();
+        public IActionResult Index()
+        {
+            return View(stockViewModels);
+        }
+
+        [HttpGet]
+        public IActionResult AddStock()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStock(AddStockViewModel StockViewModel)
+        {
+            var newStock = new StockViewModel
+            {
+                NameStock = StockViewModel.NameStock,
+                Price = StockViewModel.Price               
+            };
+
+            stockViewModels.Add(newStock);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ Net14Online/Net14Web/Models/InvestPortfolio/StockViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Net14Web.Models.InvestPortfolio
+{
+    public class StockViewModel
+    {
+        public string NameStock { get; set; }
+        public int Price { get; set; }
+        
+    }
+}
