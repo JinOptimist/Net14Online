29f61563cfd0db0e05ca1873f82eb1b7a62a7fcb
Varan5 <Varanitsa.eduard@yandex.ru>
2024-01-14 21:19:45 +0300

    Task_216 To create controller, action/view for PC Shop

---
+++ Net14Online/Net14Web/Controllers/PCSHOPController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net14Web.Models.PCSHOP;
+
+namespace Net14Web.Controllers
+{
+    public class PCSHOPController : Controller
+    {
+        public static List<AddUserViewModel> UsersViewModels = new List<AddUserViewModel>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            var users = new List<UserViewModel>();
+
+            users = UsersViewModels.ConvertAll(
+            new Converter<AddUserViewModel, UserViewModel>(UsersViewModelsToUserViewModel));
+
+            return View(users);
+        }
+        private UserViewModel UsersViewModelsToUserViewModel(AddUserViewModel pf)
+        {
+            var viewmodel = new UserViewModel();
+            viewmodel.Login = pf.Login;
+            viewmodel.Name = pf.Name;
+            return viewmodel;
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(AddUserViewModel UserModel)
+        {
+            UsersViewModels.Add(UserModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: PCSHOPController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PCSHOPController/Edit/5
+        [HttpPost]
+        public ActionResult Edit(AddUserViewModel addmodel)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PCSHOPController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PCSHOPController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Net14Online/Net14Web/Models/PCSHOP/UserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Net14Web.Models.PCSHOP
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+    }
+}
