4ff65e069efc8b3a07dd408be13ce3978cd2dea0
SashaKuznetsova <Sashaa_ku@mail.ru>
2024-01-15 13:22:47 +0100

    Task 222 Booking Website (CRUD added)

---
+++ Net14Online/Net14Web/Models/BookingWeb/UserLoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Net14Web.Models.BookingWeb
+{
+    public class UserLoginViewModel
+    {
+        public string Name { get; set; }   
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
