1654ecaac9b7637b968b7407e591b66db735d101
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-01-17 19:58:31 +0400

    add CRUD

--- Net14Online/Net14Web/Controllers/InvestPortfolioController.cs
+++ Net14Online/Net14Web/Controllers/InvestPortfolioController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Net14Web.Models.Dnd;
 using Net14Web.Models.InvestPortfolio;
 
 namespace Net14Web.Controllers
@@ -28,6 +29,18 @@ namespace Net14Web.Controllers
 
             stockViewModels.Add(newStock);
 
+            return RedirectToAction("Index");
+        }
+        public IActionResult Remove(string name)
+        {
+            var stockDelete = stockViewModels.First(x => x.NameStock == name);
+            stockViewModels.Remove(stockDelete);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult UpdatePrice(string name, int price)
+        {
+            stockViewModels.First(x => x.NameStock == name).Price = price;
             return RedirectToAction("Index");
         }
     }
