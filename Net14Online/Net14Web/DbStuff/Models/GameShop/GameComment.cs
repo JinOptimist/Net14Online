b9364e48726c4b68d439dc70787e05c366f4ff60
Alexandr <alexandrglazko@mail.ru>
2024-01-24 02:45:38 +0300

    Added Attribute

--- Net14Online/Net14Web/DbStuff/Models/GameShop/GameComment.cs
+++ Net14Online/Net14Web/DbStuff/Models/GameShop/GameComment.cs
@@ -4,11 +4,8 @@ namespace Net14Web.DbStuff.Models.GameShop
 {
     public class GameComment : BaseModel
     {
-        [Required]
-        [MaxLength(200)]
         public string Content { get; set; } = null!;
 
-        [Required]
         public Game CommentedGame { get; set; } = null!;
     }
 }
