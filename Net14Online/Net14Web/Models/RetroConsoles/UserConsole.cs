ca00a0543d5d3ec06ab99e946d03546ae36ef8b5
Pavel-Karpov-2002 <79276746+Pavel-Karpov-2002@users.noreply.github.com>
2024-01-24 00:26:02 +0300

    Revert "Revert "Merge branch 'main' of https://github.com/JinOptimist/Net14Online""
    This reverts commit dc1aef445feb6061846be66946cae0924a0ac610.

--- Net14Online/Net14Web/Models/RetroConsoles/UserConsole.cs
+++ Net14Online/Net14Web/Models/RetroConsoles/UserConsole.cs
@@ -2,10 +2,12 @@
 {
     public class UserConsole
     {
+        public int Id { get; set; }
         public string PicUrl { get; set; }
         public string Name { get; set; }
         public int NumOfConsoles {get;set;}
         public string Discription { get; set; }
         public string Email { get; set; }
+        public string Password{ get; set; }
     }
 }
