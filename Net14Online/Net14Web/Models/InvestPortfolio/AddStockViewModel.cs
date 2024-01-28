0215cc7697eb5d3c27c8fb5bd4d671f4bf2d407c
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-01-28 09:21:53 +0400

    add CheckingForPositiveNumbersAttribute

--- Net14Online/Net14Web/Models/InvestPortfolio/AddStockViewModel.cs
+++ Net14Online/Net14Web/Models/InvestPortfolio/AddStockViewModel.cs
@@ -7,7 +7,9 @@ namespace Net14Web.Models.InvestPortfolio
     {
         [Required(ErrorMessage = "Ты забыл написать название акции!")]
         [ForbiddenStocks("Аэрофлот",ErrorMessage = "Я не люлбю эту компанию, такое я создавать не буду")]
+        
         public string NameStock { get; set; }
+        [CheckingForPositiveNumbers(ErrorMessage = "Введите положительное число")]
         public int Price { get; set; }
     }
 }
