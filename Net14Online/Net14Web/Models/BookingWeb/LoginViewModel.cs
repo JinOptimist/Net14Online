016540e843324ce2092f9fa7267f75f3c7f6a214
SashaKuznetsova <Sashaa_ku@mail.ru>
2024-02-02 17:39:33 +0100

    Task 305 Booking Validation

--- Net14Online/Net14Web/Models/BookingWeb/LoginViewModel.cs
+++ Net14Online/Net14Web/Models/BookingWeb/LoginViewModel.cs
@@ -1,9 +1,20 @@
-﻿namespace Net14Web.Models.BookingWeb
+﻿using Net14Web.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net14Web.Models.BookingWeb
 {
     public class LoginViewModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "The name shouldnt be longer than 10 symbols!")]
+        [ForbiddenNameAttribute(ErrorMessage = "Name Name is forbidden")]
         public string Name { get; set; }   
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "The password is too short!")]
         public string Password { get; set; }
     }
 }
---
+++ Net14Online/Net14Web/Models/ValidationAttributes/ForbiddenNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Net14Web.Models.ValidationAttributes
+{
+    public class ForbiddenNameAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+
+            if (value is not string && value is not null)
+            {
+                throw new ArgumentException("ForbidenSymbolsAttribute can be only string");
+            }
+
+            var stringValue = (string)value;
+            return stringValue != "Name";
+        }
+    }
+}
