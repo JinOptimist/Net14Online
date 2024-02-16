1bb880f076c3339780f366ea6aff6eef53d8d1e4
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-02-16 10:30:05 +0400

    add validation bonds

--- Net14Online/Net14Web/Models/Bonds/AddBondsViewModel.cs
+++ Net14Online/Net14Web/Models/Bonds/AddBondsViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace Net14Web.Models.Bonds
+﻿using Net14Web.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net14Web.Models.Bonds
 {
     public class AddBondsViewModel
     {
-        public int Id { get; set; }
+        [Required (ErrorMessage ="Название обязательно")]
+
         public string Name { get; set; }
+
+        public int Id { get; set; }
+        
+        [CheckingForPositiveNumbers(ErrorMessage = "Вот ты фокусник")]
         public int Price { get; set; }
 
     }
