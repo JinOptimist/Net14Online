bc744245a8f25064520bc09a5cfa9e0273a958dd
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-02-16 07:52:05 +0400

    fix

--- Net14Online/Net14Web/DbStuff/WebDbContext.cs
+++ Net14Online/Net14Web/DbStuff/WebDbContext.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Net14Web.DbStuff.Models;
+using Net14Web.DbStuff.Models.Bonds;
 using Net14Web.DbStuff.Models.BookingWeb;
 using Net14Web.DbStuff.Models.GameShop;
 using Net14Web.DbStuff.Models.InvestPort;
 using Net14Web.DbStuff.Models.LifeScore;
 using Net14Web.DbStuff.Models.Movies;
+using Net14Web.DbStuff.Models.PcShop;
 using Net14Web.DbStuff.Models.RetroConsoles;
 using Net14Web.DbStuff.Models.TaskTracker;
-using Net14Web.DbStuff.Models.InvestPort;
-using Net14Web.DbStuff.Models.PcShop;
-using Net14Web.DbStuff.Models.Bonds;
 namespace Net14Web.DbStuff
 {
     public class WebDbContext : DbContext
@@ -17,7 +16,7 @@ namespace Net14Web.DbStuff
         public DbSet<Hero> Heroes { get; set; }
         public DbSet<Weapon> Weapons { get; set; }
         public DbSet<UsersPcShop> UserPcShop { get; set; }
-        public DbSet<PCModel> PCModel { get; set;}
+        public DbSet<PCModel> PCModel { get; set; }
         public DbSet<CpuModel> CpuModel { get; set; }
         public DbSet<Search> Searches { get; set; }
         public DbSet<Game> Games { get; set; }
@@ -32,7 +31,7 @@ namespace Net14Web.DbStuff
         public DbSet<LoginBooking> LoginsBooking { get; set; }
         public DbSet<Bond> Bonds { get; set; }
         public DbSet<Coupon> Coupons { get; set; }
-        
+
         // LifeScore
         public DbSet<SportGame> SportGames { get; set; }
         public DbSet<Team> Teams { get; set; }
