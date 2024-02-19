fc5dd1e8ba416f01783b60d309feb796afe7f3f9
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-02-19 21:18:24 +0300

    First example of JS

---
+++ Net14Online/Net14Web/wwwroot/js/dnd/index.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+
+    $('.hero .just-name,.hero .hero-name').click(function () {
+        const heroBlock = $(this).closest('.hero');
+        heroBlock.toggleClass('short');
+        heroBlock.toggleClass('full');
+    });
+  
+});
