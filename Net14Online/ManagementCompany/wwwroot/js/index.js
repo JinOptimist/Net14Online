a30bb93f014d1222f86a07291aefce5edf162d55
AASaln89 <94790664+AASaln89@users.noreply.github.com>
2024-02-21 18:58:03 +0300

    Task 358

---
+++ Net14Online/ManagementCompany/wwwroot/js/index.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+
+    $('.task-table.short-table, .task-table.full-table').click(function() {
+        const heroBlock = $(this).closest('.task-table');
+        heroBlock.toggleClass('short-table');
+        heroBlock.toggleClass('full-table');
+    });
+});
