991b9af60d56740e95dee18f4872bc4d8e5676e7
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-02-26 20:45:22 +0300

    Add chat to home index page

---
+++ Net14Online/ChatMiniService/SignalRHubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatMiniService.SignalRHubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string userName, string message)
+        {
+            Clients.All.SendAsync("ServerGotOneNewMessage", userName, message).Wait();
+        }
+
+        public void NewUserEnterToChat(string userName)
+        {
+            Clients.All.SendAsync("OneMoreUserEnterToChat", userName).Wait();
+        }
+    }
+}
---
+++ Net14Online/Net14Web/wwwroot/js/home/chat.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    const userName = $('.user-name').val();
+
+    const hub = new signalR.HubConnectionBuilder()
+        .withUrl("https://localhost:7280/chat")
+        .build();
+
+    // Something happend on server side
+    hub.on('ServerGotOneNewMessage', function (user, message) {
+        addMessage(user, message);
+        //console.log("user: " + user + " message: " + message);
+    });
+
+    hub.on('OneMoreUserEnterToChat', function (user) {
+        addMessage('', user + ' enter to chat')
+    });
+
+    $('.send-message-button').click(function () {
+        const message = $('.new-message-text').val();
+        // Something happedn on client side
+        // Call server
+        hub.invoke('SendMessage', userName, message);
+    });
+
+    hub.start()
+        .then(function () {
+            hub.invoke('NewUserEnterToChat', userName);
+        });
+
+
+    function addMessage(user, message) {
+        const newMessageBlock = $('.message.template').clone();
+        newMessageBlock.removeClass('template');
+        newMessageBlock.find('.user-name').text(user);
+        newMessageBlock.find('.message-text').text(message);
+
+        $('.messages').append(newMessageBlock);
+    }
+});
