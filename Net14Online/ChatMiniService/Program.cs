f769a60d36e951296c9eed0efe39ff9de386c9dc
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-02-28 21:05:28 +0300

    Add Custom Auth to ChatApi

--- Net14Online/ChatMiniService/Program.cs
+++ Net14Online/ChatMiniService/Program.cs
@@ -1,9 +1,16 @@
 using ChatMiniService.DbStuff;
+using ChatMiniService.FluentValidators;
+using ChatMiniService.Middlewares;
+using ChatMiniService.Services;
 using ChatMiniService.SignalRHubs;
+using ChatMiniService.ViewModels;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<FakeDb>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<IValidator<MessageViewModel>, MessageViewModelValidator>();
 
 builder.Services.AddCors(option =>
 {
@@ -21,6 +28,8 @@ builder.Services.AddSignalR();
 var app = builder.Build();
 
 app.UseCors();
+
+app.UseMiddleware<ChatAuthMiddleware>();
 
 app.MapHub<ChatHub>("/chat");
 
