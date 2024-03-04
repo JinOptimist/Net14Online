d88d0bf1890c1104732d9ccbf3f2bba323ea370b
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-03-04 20:35:37 +0300

    E2E test for DND

---
+++ Net14Online/Net14Web.E2ETests/PagesSelector/DndIndexPage.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+
+namespace Net14Web.E2ETests.PagesSelector
+{
+    public class DndIndexPage
+    {
+        public static By FirstHeroShortName => By.CssSelector(".hero.short:first-child .just-name");
+        public static By FullHeroBlock => By.CssSelector(".hero.full");
+        public static By HeroCoinsCount => By.CssSelector(".coin-block .coins-count");
+        public static By HeroCoinsIcon => By.CssSelector(".coin-block .icon");
+    }
+}
---
+++ Net14Online/Net14Web.E2ETests/PagesSelector/LoginPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace Net14Web.E2ETests.PagesSelector
+{
+    internal class LoginPage
+    {
+        public static By UserNameInput => By.CssSelector("#UserName");
+        public static By PasswordInput => By.CssSelector("#Password");
+        public static By SubmitButton => By.CssSelector("[type=submit]");
+    }
+}
