4749a96acc9d36b1e339b8e95549317fc11e7f38
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-03-07 10:34:37 +0400

    upgrade click on bond name

--- Net14Online/Net14Web/wwwroot/js/Bond/index.js
+++ Net14Online/Net14Web/wwwroot/js/Bond/index.js
@@ -2,9 +2,11 @@
 
 $(document).ready(function () {
 
-
     $('.bond-name').click(function () {
+        $('.delete-bond').addClass('short');
+
         const bondBlock = $(this).closest('.chapter-bond').find('.delete-bond');
-        bondBlock.toggleClass('short');
-    });   
+        bondBlock.removeClass('short');
+    });
+
 });
