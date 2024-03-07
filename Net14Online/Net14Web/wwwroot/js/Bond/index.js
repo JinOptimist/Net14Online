b340695a5734b1f8d0258e1a94f264ff48a82c69
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-03-07 11:02:32 +0400

    add js on bond-price

--- Net14Online/Net14Web/wwwroot/js/Bond/index.js
+++ Net14Online/Net14Web/wwwroot/js/Bond/index.js
@@ -4,9 +4,17 @@ $(document).ready(function () {
 
     $('.bond-name').click(function () {
         $('.delete-bond').addClass('short');
+        $('.price-change-field').addClass('short');
 
-        const bondBlock = $(this).closest('.chapter-bond').find('.delete-bond');
-        bondBlock.removeClass('short');
+        const bondNameBlock = $(this).closest('.chapter-bond').find('.delete-bond');
+        bondNameBlock.removeClass('short');
     });
 
+    $('.bond-price').click(function () {
+        $('.price-change-field').addClass('short');
+        $('.delete-bond').addClass('short');
+
+        const bondPriceBlock = $(this).closest('.chapter-bond').find('.price-change-field');
+        bondPriceBlock.removeClass('short');
+    });
 });
