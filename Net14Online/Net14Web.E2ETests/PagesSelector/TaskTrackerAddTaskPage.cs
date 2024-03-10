ac795c4c3dff9216d2b59b3491a90783b4487805
Vasileva Maria <buziuka@yandex.ru>
2024-03-10 17:17:30 +0100

    Add E2E test for TaskTracker
    Add create new Task E2E test

---
+++ Net14Online/Net14Web.E2ETests/PagesSelector/TaskTrackerAddTaskPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace Net14Web.E2ETests.PagesSelector
+{
+    public class TaskTrackerAddTaskPage
+    {
+        public static By AddTaskNameInput => By.CssSelector(".add-form__input_name");
+        public static By AddTaskDescriptionInput => By.CssSelector(".add-form__input_description");
+        public static By AddTaskSubmitButton => By.CssSelector(".add-form__button");
+    }
+}
