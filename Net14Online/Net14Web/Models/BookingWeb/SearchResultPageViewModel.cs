82c8b85467845042ce4f9d2cf3d73f8e48153e63
SashaKuznetsova <Sashaa_ku@mail.ru>
2024-04-24 10:24:11 +0100

    fixes for booking

---
+++ Net14Online/Net14Web/Models/BookingWeb/SearchResultPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Net14Web.Models.BookingWeb
+{
+    public class SearchResultPageViewModel
+    {
+        public string? UserName { get; set; }
+        public List<SearchResultViewModel> SearchResults { get; set; }
+    }
+}
+
+
